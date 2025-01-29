16880b1cad1fa9b193f32e2323d033a824cb7644
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-01-29 09:52:49 -0700

    python practice

---
+++ variables/practice.py
@@ -0,0 +1,11 @@
+# Gavin Woodhouse, Variables Practice Python
+myname = ("Gavin")
+oneten = ("8")
+hunthou = ("456")
+breakfast = ("poptart")
+color = ("orange & purple")
+school = ("UCAS")
+year = 2025
+eye = ("brown")
+age = 15
+subject = ("CS :)")
