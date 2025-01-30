f7cd0135c36ba09f6000f556b3aafdc1fa48b001
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-01-30 09:54:06 -0700

    template finish

--- template.c
+++ template.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+char name[] = "Gavin";
+int num = 42;
+float pi = 3.14;
+
 int main(void){
-    printf("hello world");
+    printf("Hello %s, my age is %d and my favorite number is %f\n", name, num, pi);
+    printf("%d\n", num);
+    printf("%f\n", pi);
     return 0;
 }
--- variables/practice.py
+++ variables/practice.py
@@ -1,7 +1,7 @@
 # Gavin Woodhouse, Variables Practice Python
 myname = ("Gavin")
-oneten = ("8")
-hunthou = ("456")
+oneten = 8
+hunthou = 456
 breakfast = ("poptart")
 color = ("orange & purple")
 school = ("UCAS")
@@ -9,3 +9,4 @@ year = 2025
 eye = ("brown")
 age = 15
 subject = ("CS :)")
+print (myname, oneten, hunthou, breakfast, color, school, year, eye, age, subject)
