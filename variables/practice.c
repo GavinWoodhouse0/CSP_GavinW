d74878476baa08b8f15eeec9516be2919bae3d81
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-01-31 09:28:00 -0700

    complete practice

---
+++ variables/practice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char myname[] = "Gavin";
+int oneten = 8;
+int hunthou = 456;
+char breakfast[] = "poptart";
+char color[] = "orange & purple";
+char school[] = "UCAS";
+int year = 2025;
+char eye[] = "brown";
+int age = 15;
+char subject[] = "CS :)";
+
+int main(void){
+    printf("%s %d %d %s %s %s %d %s %d %s\n", myname, oneten, hunthou, breakfast, color, school, year, eye, age, subject);
+    return 0;
+}
