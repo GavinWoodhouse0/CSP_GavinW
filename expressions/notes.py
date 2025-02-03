4ebe31f416e577f05621dd9baa7852bfc5dff858
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-03 09:53:39 -0700

    experession notes

---
+++ expressions/notes.py
@@ -0,0 +1,11 @@
+#Gavin Woodhouse, Notes Python
+#integers are numbers with no decimal points
+#floats are numbers with decimal points
+#algorithms are a set of instructions to complete a task
+print(10+5)
+print(5.5-5)
+print(7.5*5)
+print(2/5)
+print(5**2) #exponents
+print(10.5//5) #division, then rounded
+print(5.25%5) #Modulo/Modulus/MOD gives the remainder
--- variables/first_project.py
+++ variables/first_project.py
@@ -1,3 +1,7 @@
 #Gavin Woodhouse, First Program in Python
 
 food = input("Tell me, what's your favorite food?: ")
+
+print(food+ "... I see.")
+
+print("Strange, but okay.")
