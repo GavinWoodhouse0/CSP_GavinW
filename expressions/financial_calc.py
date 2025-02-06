be92f8c04ab996a236667e29ebeddc92a8cfb1a2
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-06 09:46:21 -0700

    expression practice c

--- expressions/financial_calc.py
+++ expressions/financial_calc.py
@@ -1,1 +1,39 @@
 #Gavin Woodhouse, Financial Calculator Python
+
+#write a print statement telling the user what the program is (Budget calculator)
+
+#Ask for monthly income (User input)
+
+#Ask for rent amount (User input)
+
+#Ask for utilities amount (User input)
+
+#Ask for groceries income (User input)
+
+#Ask for transportation income (User input)
+income = float(input("what is your monthly income\n"))
+#Calcutlate savings as 10% of income (variable)
+
+#Calcutlate spending money income -(rent+utilities+groceries+transportation+savings) (variable)
+
+#Calcutlate percent of rent (rent/income) (variable)
+
+#Calcutlate percent of utilities (utilities/income) (variable)
+
+#Calcutlate percent of groceries (groceries/income) (variable)
+
+#Calcutlate percent of transportation (transportation/income) (variable)
+
+#Calcutlate percent of spending money (spending money/income) (variable)
+
+# tell user catergory spending amount AND percent for rent ("You spend $XX.XX on rent and that is XX% of your income")
+
+# tell user catergory spending amount AND percent for utilities ("You spend $XX.XX on utilities and that is XX% of your income")
+
+# tell user catergory spending amount AND percent for groceries ("You spend $XX.XX on groceries and that is XX% of your income")
+
+# tell user catergory spending amount AND percent for transportation ("You spend $XX.XX on transportation and that is XX% of your income")
+
+# tell user catergory spending amount AND percent for spending ("You spend $XX.XX on spending and that is XX% of your income")
+
+# tell user catergory spending amount AND percent for saving ("You spend $XX.XX on savings and that is 10% of your income")
