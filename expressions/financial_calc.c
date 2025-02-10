4b7456ab3f6ffc1bbafe5cb76e956909e6726c7f
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-10 09:53:24 -0700

    python strings notes

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -1,7 +1,11 @@
 //Gavin Woodhouse, Fianacial Calculator C
 #include <stdio.h>
 
+float income;
+
 int main(void){
-    printf("Hello World");
+    printf("This is a budget calculator. Type in your monthly spending to how much of your income you spend.");
+    printf("What is your monthly income?\n");
+    scanf("%f", &income);
     return 0;
 }
---
+++ strings/notes.py
@@ -0,0 +1,9 @@
+#Gavin Woodhouse, Strings Notes Python
+# Words in programming (data type) "" ''
+
+name = input("What is your name?\n").strip().capitalize()
+
+print(f"Hello {name} welcome to my program.")
+
+sentence = "The quick brown fox"
+print(sentence.find("fox"))
