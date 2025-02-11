ebabe701c3a013fb67ea96ccfff9f8843d1cf8e3
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-11 09:53:47 -0700

    finance calc c wip

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -6,14 +6,6 @@ float rent;
 float utilities;
 float groceries;
 float transportation;
-float savings = income/10;
-float spending = rent+utilities+groceries+transportation+savings;
-float rent_percent = rent/income*100;
-float utilities_percent = utilities/income*100;
-float groceries_percent = groceries/income*100;
-float transportation_percent = transportation/income*100;
-float savings_percent = savings/income*100;
-float spending_percent = spending/income*100;
 
 int main(void){
     printf("This is a budget calculator. Type in your monthly spending to how much of your income you spend.");
@@ -27,6 +19,20 @@ int main(void){
     scanf("%f", &groceries);
     printf("How much do you spend on transportation every month?\n");
     scanf("%f", &transportation);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent)
+    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
+    printf("You spend $%.2f on utlitities and that is %f% of your income\n", utilities, utilities_percent);
+    printf("You spend $%.2f on rent and that is %f% of your income\n", groceries, groceries_percent);
+    printf("You spend $%.2f on rent and that is %f% of your income\n", transportation, transportation_percent);
+    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
+    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
     return 0;
 }
+
+float savings = income/10;
+float spending = rent+utilities+groceries+transportation+savings;
+float rent_percent = rent/income*100;
+float utilities_percent = utilities/income*100;
+float groceries_percent = groceries/income*100;
+float transportation_percent = transportation/income*100;
+float savings_percent = savings/income*100;
+float spending_percent = spending/income*100;
