a41f1e729dcb4af865aef01eef7d33d69ea42f50
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-12 09:17:54 -0700

    finished finance calc in c

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -8,7 +8,7 @@ float groceries;
 float transportation;
 
 int main(void){
-    printf("This is a budget calculator. Type in your monthly spending to how much of your income you spend.");
+    printf("This is a budget calculator. Type in your monthly spending to how much of your income you spend.\n");
     printf("What is your monthly income?\n");
     scanf("%f", &income);
     printf("What is your monthly rent?\n");
@@ -19,20 +19,19 @@ int main(void){
     scanf("%f", &groceries);
     printf("How much do you spend on transportation every month?\n");
     scanf("%f", &transportation);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
-    printf("You spend $%.2f on utlitities and that is %f% of your income\n", utilities, utilities_percent);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", groceries, groceries_percent);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", transportation, transportation_percent);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
-    printf("You spend $%.2f on rent and that is %f% of your income\n", rent, rent_percent);
+    float savings = income/10;
+    float spending = rent+utilities+groceries+transportation+savings;
+    float rent_percent = rent/income*100;
+    float utilities_percent = utilities/income*100;
+    float groceries_percent = groceries/income*100;
+    float transportation_percent = transportation/income*100;
+    float savings_percent = savings/income*100;
+    float spending_percent = spending/income*100;
+    printf("You spend $%.2f on rent and that is %.2f percent of your income\n", rent, rent_percent);
+    printf("You spend $%.2f on utlitities and that is %.2f percent of your income\n", utilities, utilities_percent);
+    printf("You spend $%.2f on groceries and that is %.2f percent of your income\n", groceries, groceries_percent);
+    printf("You spend $%.2f on transportation and that is %.2f percent of your income\n", transportation, transportation_percent);
+    printf("You save $%.2f and that is %.2f percent of your income\n", savings, savings_percent);
+    printf("You spend $%.2f in total and that is %.2f percent of your income\n", spending, spending_percent);
     return 0;
 }
-
-float savings = income/10;
-float spending = rent+utilities+groceries+transportation+savings;
-float rent_percent = rent/income*100;
-float utilities_percent = utilities/income*100;
-float groceries_percent = groceries/income*100;
-float transportation_percent = transportation/income*100;
-float savings_percent = savings/income*100;
-float spending_percent = spending/income*100;
