afb05f9d11b0cec42bdb18a9ba25ec9bd79c620c
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-19 09:52:25 -0700

    updated python hello world

--- functions/notes.c
+++ functions/notes.c
@@ -7,19 +7,9 @@ void add(int numOne,int numTwo){
     printf("%d\n", numOne+numTwo);
 }
 
-char input(char type[20]){
-    char answer [50];
-    printf("Please give me a %s:\n", type);
-    scanf("%s", answer);
-    return answer;
-}
-
 int main(void){
-    //printf("Hello World\n");
+    printf("Hello World\n");
     //add();
-    input("name");
-    input("verb");
-    input("place");
-    printf("%s was %s until they somehow reached %s",input("name"),input("verb"),input("place"));
+    add(4,9);
     return 0;
 }
---
+++ functions/update_world.py
@@ -0,0 +1,10 @@
+#Gavin Woodhouse, Updated Hello World Program
+
+def hello(name):
+    print("Hello,", name)
+
+hello("Gavin")
+hello("Grace")
+hello("Vienna")
+hello("Alastor")
+hello("John")
