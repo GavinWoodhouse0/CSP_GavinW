f8f00cf69c9f1d2da59bb9ffc2fac2af523c6f0b
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-24 09:53:33 -0700

    started conditionals notes

---
+++ conditionals/notes.py
@@ -0,0 +1,30 @@
+#Gavin Woodhouse, Conditionals Notes Python
+
+#print("Hello, welcome to my program that will sort you into a catergory")
+
+#name = input("What is your name:\n")
+
+#if name == "Gavin":
+    #print("You can't be me, I'm me!")
+#else:
+    #print("Alright!")
+
+
+
+#num = int(input("How many would you like (give me a number above 0):\n"))
+
+#if num == 1:
+    #print("You have asked for an item")
+#elif num <= 3:
+    #print("You have asked for a couple of items")
+#elif num <= 5:
+    #print("You have asked for a few of items or are southern and asked for a couple of items")
+#else:
+    #print("You have asked for some items")
+
+name = "Gavin"
+
+if "a" in name or "e" in name or "i" in name or "o" in name or "u" in name:
+    print("Your name has a vowel!")
+else:
+    print("Your name doesn't have a vowel.")
