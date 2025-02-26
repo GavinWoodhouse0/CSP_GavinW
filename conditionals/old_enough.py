efaa763b5c6e26b20c0a5926fe5bb346f660612c
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-02-26 09:53:20 -0700

    started work on new old enough program

---
+++ conditionals/old_enough.py
@@ -0,0 +1,12 @@
+#Gavin Woodhouse, Old Enough Python
+
+age = int(input("How old are you?\n"))
+
+if age >= 21:
+    print("You're old enought to drink! This is a VERY important age. Be responsible.")
+elif age >= 18:
+    print("You are now a legal adult, which allows you to do so much that it would take ages to list them all.")
+elif age >= 15:
+    print("Depending on who and where you are, you're probably able to drive!")
+elif age >= 13:
+    print("You're probably in junior high now or at least will soon be, but more importantly you can enjoy at lot more movies and video games.")
