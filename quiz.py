d7904c7b019cf93a54f7e58f2912aecbc7e48940
GavinWoodhouse0 <gavin.woodhouse@ucas-edu.net>
2025-03-20 15:46:50 +0000

    Co-authored-by: themonky-monky <themonky-monky@users.noreply.github.com> Co-authored-by: TatianaSusov <TatianaSusov@users.noreply.github.com> Co-authored-by: AliWalton580 <AliWalton580@users.noreply.github.com>

---
+++ quiz.py
@@ -0,0 +1,12 @@
+#Gavin Woodhouse, Tatiana Susov, Andrea Lugo, Ali Walton, Quiz
+
+#Function for score table, makes sure that after each question that is correct a point is added, score will be printed at end of game.
+
+#Function for questions, Gavin
+def question(prompt):
+    return input(f"{prompt}?\n")
+question("Can you tell me the time")
+
+#Loop to print questions with a random function, Ali
+
+#Conditional to say whether answer to question was correct or not, Tatiana
