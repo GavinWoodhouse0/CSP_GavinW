3c6305070dd6cb5ca22bc0d521f4ae6b4e01b08a
GavinWoodhouse0 <gavinwoodhouse@ucas-edu.net>
2025-03-21 09:53:26 -0600

    Co-authored-by: AliWalton580 <AliWalton580@users.noreply.github.com> Co-authored-by: TatianaSusov <TatianaSusov@users.noreply.github.com> Co-authored-by: themonky-monky <themonky-monky@users.noreply.github.com>

--- quiz.py
+++ quiz.py
@@ -1,13 +1,24 @@
 #Gavin Woodhouse, Tatiana Susov, Andrea Lugo, Ali Walton, Quiz
 import time
 #Function for score table, makes sure that after each question that is correct a point is added, score will be printed at end of game.
-
+score = 0 
 #Function for questions, Gavin
-def question(prompt, answer):
+
+def question(prompt):
     return input(f"{prompt}?\n")
-    
-#question("Can you tell me the time")
+
+def answer(correct):
+    if guess == correct:
+        print("cool")
+        score +=1      
+    else:
+        print("nah")
+guess = question("Can you tell me the time")
+answer("yes")    
 
 #Loop to print questions with a random function, Ali
+import random
 
+num=1
+rand = random.randint(1,19)
 #Conditional to say whether answer to question was correct or not, Tatiana
