392936f719a62ba8eda1660f38f3dc14e98c35d5
GavinWoodhouse0 <gavin.woodhouse@ucas-edu.net>
2025-04-24 09:36:36 -0600

    Finished Javascript notes.

--- javascript/notes.js
+++ javascript/notes.js
@@ -42,3 +42,13 @@ function highlight(){
 function pop(){
     window.alert("For real. Don't click this!")
 }
+
+function more(){
+    if (document.getElementById("extra").style.display != "flex") {
+        document.getElementById("extra").style.display = "flex"
+        document.getElementById("shw").innerHTML = "Show Less"
+    } else {
+        document.getElementById("extra").style.display = "none"
+        document.getElementById("shw").innerHTML = "Show More"
+    } 
+}
