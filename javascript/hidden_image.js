e8c1f56a2de637c3852b0f0798f66380b7035cee
GavinWoodhouse0 <gavin.woodhouse@ucas-edu.net>
2025-04-28 09:36:09 -0600

    Finished Hidden Image JavaScript project.

---
+++ javascript/hidden_image.js
@@ -0,0 +1,7 @@
+function more(){
+    if (document.getElementById("extra").style.display != "flex") {
+        document.getElementById("extra").style.display = "flex"
+    } else {
+        document.getElementById("extra").style.display = "none"
+    } 
+}
